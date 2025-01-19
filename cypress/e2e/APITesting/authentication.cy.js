d152228d61dd71c12498f36f9b52bcb613868a93
Rajamegam <rajamegam7@gmail.com>
2025-01-19 15:51:46 +0530

    authentication

--- cypress/e2e/APITesting/authentication.cy.js
+++ cypress/e2e/APITesting/authentication.cy.js
@@ -1,78 +1,78 @@
-describe("Authentication", () => {
-    it("basic auth", () => {
-        cy.request({
-            method: 'GET',
-            url: 'https://postman-echo.com/basic-auth',
-            auth: {
-                username: 'postman',
-                password: 'password'
-            }
-        })
-            .then((response) => {
-                expect(response.status).to.eq(200)
-                expect(response.body.authenticated).to.be.true
+// describe("Authentication", () => {
+//     it("basic auth", () => {
+//         cy.request({
+//             method: 'GET',
+//             url: 'https://postman-echo.com/basic-auth',
+//             auth: {
+//                 username: 'postman',
+//                 password: 'password'
+//             }
+//         })
+//             .then((response) => {
+//                 expect(response.status).to.eq(200)
+//                 expect(response.body.authenticated).to.be.true
 
-            })
-    })
-    it("Digest auth", () => {
-        cy.request({
-            method: 'GET',
-            url: 'https://postman-echo.com/basic-auth',
-            auth: {
-                username: 'postman',
-                password: 'password',
-                method: 'Digest'
-            }
-        })
-            .then((response) => {
-                expect(response.status).to.eq(200)
-                expect(response.body.authenticated).to.be.true
+//             })
+//     })
+//     it("Digest auth", () => {
+//         cy.request({
+//             method: 'GET',
+//             url: 'https://postman-echo.com/basic-auth',
+//             auth: {
+//                 username: 'postman',
+//                 password: 'password',
+//                 method: 'Digest'
+//             }
+//         })
+//             .then((response) => {
+//                 expect(response.status).to.eq(200)
+//                 expect(response.body.authenticated).to.be.true
 
-            })
-    })
-    const token = ''
-    it("Bearer token", () => {
+//             })
+//     })
+//     const token = ''
+//     it("Bearer token", () => {
 
-        cy.request({
-            method: 'GET',
-            url: 'https://api.github.com/user/repos',
-            headers: {
-                Authorization: 'Bearer ' + token
-            }
-        })
-            .then((response) => {
-                expect(response.status).to.eq(200)
+//         cy.request({
+//             method: 'GET',
+//             url: 'https://api.github.com/user/repos',
+//             headers: {
+//                 Authorization: 'Bearer ' + token
+//             }
+//         })
+//             .then((response) => {
+//                 expect(response.status).to.eq(200)
 
-            })
+//             })
 
-    })
-    it.only("oauth2.0", () => {
+//     })
+//     it.only("oauth2.0", () => {
 
-        cy.request({
-            method: 'POST',
-            url: 'https://api-m.sandbox.paypal.com/v1/oauth2/token',
-            auth: {
-                username: '',
-                password: ''
-            },
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
+//         cy.request({
+//             method: 'POST',
+//             url: 'https://api-m.sandbox.paypal.com/v1/oauth2/token',
+//             auth: {
+//                 username: '',
+//                 password: ''
+//             },
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded'
 
-            },
-            body: {
+//             },
+//             body: {
 
-                "grant_type": "client_credentials",
-                "ignorecache": "true",
-                "return_auth_schemas": "true",
-                "return_client_metadata": "true",
-                "return_unconsented_scopes": "true"
+//                 "grant_type": "client_credentials",
+//                 "ignorecache": "true",
+//                 "return_auth_schemas": "true",
+//                 "return_client_metadata": "true",
+//                 "return_unconsented_scopes": "true"
 
-            }
-        })
-            .then((response) => {
-                cy.log(JSON.stringify(response.body))
-                const access_token = response.body.access_token
-                cy.log('access_token: '+access_token)
-            })
-    })
-})
+//             }
+//         })
+//             .then((response) => {
+//                 cy.log(JSON.stringify(response.body))
+//                 const access_token = response.body.access_token
+//                 cy.log('access_token: '+access_token)
+//             })
+//     })
+// })
